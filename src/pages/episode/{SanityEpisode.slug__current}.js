234f6fa777bf93fe5470546b417af043c94d54dd
Subham Bharadwaz <subhamsbharadwaz@gmail.com>
2021-08-14 08:34:14 +0530

    :bug: fix(image null)

--- src/pages/episode/{SanityEpisode.slug__current}.js
+++ src/pages/episode/{SanityEpisode.slug__current}.js
@@ -26,10 +26,12 @@ export default function SanityEpisode({ data }) {
   const episode = data.sanityEpisode;
   return (
     <Layout title={episode.title} description={episode.description}>
-      <GatsbyImage
-        image={episode.image.asset.gatsbyImageData}
-        alt={episode.title}
-      />
+      {episode.image?.asset?.gatsbyImageData && (
+        <GatsbyImage
+          image={episode.image.asset.gatsbyImageData}
+          alt={episode.title}
+        />
+      )}
       <h1>{episode.title}</h1>
       <p>
         (posted {episode.data}) - {episode.description}
